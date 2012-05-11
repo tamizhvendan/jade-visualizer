e948eda139d861a7496f75fac09fd141e78f5e48
Tamizhvendan S <tamizh88@gmail.com>
2012-05-11 15:46:24 +0530

    Html output display beautified

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var http = require('http'),
 	jade = require('jade'),
-	socketIO = require('socket.io'),
+	socketIO = require('socket.io'),    
 	staticFileProvider = require('./lib/staticFileProvider'),
 	port = process.env.PORT || 2222,
 	server = {},
 	io = {};
 
 var parseJadeText = function (jadeTemplate) {
-	console.log(jadeTemplate);
-	return jade.compile(jadeTemplate)();
+    var os = require('os'),
+        eol = os.platform() === 'win32' ? '\r\n' : '\n',
+        jadeTemplateWithEol = jadeTemplate.join(eol);
+
+    return jade.compile(jadeTemplateWithEol, { pretty : true })();
 };
 
 server = http.createServer(function (req, res) {
@@ -21,8 +24,10 @@ console.log('Jade-Visualizer is listening @ Port : ' + port);
 io = socketIO.listen(server);
 
 io.sockets.on('connection', function (socket) {
-	socket.on('translate', function (jadeTemplate) {
-		var parsedText = parseJadeText(jadeTemplate);
-		socket.emit('output', parsedText);
-	});
+    
+    socket.on('translate', function (jadeTemplate) {
+        var parsedText = parseJadeText(jadeTemplate);
+        socket.emit('output', parsedText);
+    });
+
 });
--- static/scripts/jadeVisualizer.js
+++ static/scripts/jadeVisualizer.js
@@ -1,17 +1,33 @@
 $(function () {
-	var $output = $('#output'),
+    var $output = $('#output'),
 		$translate = $('#translate'),
-		$jadeText = $('#jadeTemplate'),
-		socket = io.connect();
+		$jadeTemplate = $('#jadeTemplate'),
+		socket = io.connect(),
+        tabKeyCode = 9;
 
+    $jadeTemplate.val('');
 
-	$translate.on('click', function (event) {
-		socket.emit('translate', $jadeText.val().trim());
-		event.preventDefault();
-	});
+    function handleTabKeyPress(event) {
+        var jadeText = $jadeTemplate.val();
+        jadeText += '\t';
+        $jadeTemplate.val(jadeText);
+        event.preventDefault();
+    }
 
-	socket.on('output', function (translatedText) {
-		$output.text(translatedText);
-		prettyPrint();
-	});
+    $jadeTemplate.on('keydown', function (event) {
+        if (event.keyCode === tabKeyCode) {
+            handleTabKeyPress(event);
+        }
+    });
+
+    $translate.on('click', function (event) {
+        var jadeText = $jadeTemplate.val().split('\n');
+        socket.emit('translate', jadeText);
+        event.preventDefault();
+    });
+
+    socket.on('output', function (translatedText) {
+        $output.text(translatedText);
+        prettyPrint();
+    });
 });
